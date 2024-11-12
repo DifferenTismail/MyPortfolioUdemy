a0599c32061639bfcade14ddecd4452431ec62f7
İsmail Baran <ismailbaran04@gmail.com>
2024-11-12 20:21:37 +0300

    otomatik günellemeler işlendi

--- MyPortfolioUdemy/obj/Debug/net6.0/MyPortfolioUdemy.AssemblyInfo.cs
+++ MyPortfolioUdemy/obj/Debug/net6.0/MyPortfolioUdemy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolioUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2a05ba1e5a2337870685f49035f13e089e53aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolioUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolioUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
